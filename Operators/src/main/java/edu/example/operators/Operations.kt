b2f9ac1df5290704f01576e4e6f73ce6020c07f7
nemax364-blip <nemax364@gmail.com>
2026-03-03 08:48:52 -0600

    Operators

--- Operators/src/main/java/edu/example/operators/Operations.kt
+++ Operators/src/main/java/edu/example/operators/Operations.kt
@@ -1,22 +1,35 @@
 package edu.example.operators
 
 fun main() {
+
+    println(" Matemáticas" )
     println(1 + 1)
     println(53 - 3)
     println(50 / 10)
+    println(1 / 2)
     println(1.0 / 2.0)
     println(6 * 50)
-    println(2.times(3))
 
-    val i = 6
-    println(i.toByte())
+
+    println(" Métodos")
+    val numero = 2
+    println(numero.times(3))
+
+
+    println("Conversiones")
+    val i: Int = 6
+    val b1 = i.toByte()
+    println(b1)
 
     val oneMillion = 1_000_000
     println(oneMillion)
 
+
     var fish = 1
     fish = 2
-    println(fish)
+    println("Peces: $fish")
+
+    val aquarium = 1
 
     val numberOfFish = 5
     val numberOfPlants = 12
